0339b3ba417c664d71f69cde9cdf5be3fa7b701e
Didier Bieler <didier.bieler@epfl.ch>
2017-11-27 11:13:48 +0100

    Created the structure of the project in Clion

---
+++ cmake-build-debug/CMakeFiles/PCSC2017_Group4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PCSC2017_Group4.dir/main.cpp.o"
+  "PCSC2017_Group4.pdb"
+  "PCSC2017_Group4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PCSC2017_Group4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
