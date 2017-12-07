2ba3182655b3e6ecbeb9317b19abd962631f831d
shernand <sergio.hernandez@epfl.ch>
2017-12-07 18:39:41 +0100

    Added the prewitt edge detection filter (which is a band pass filter)

--- Project/MeanFilter.h
+++ Project/MeanFilter.h
@@ -4,8 +4,10 @@
 
 #ifndef PROJECT_MEANFILTER_H
 #define PROJECT_MEANFILTER_H
-
-
+/** Class which defines a mean filter in the spatial domain.
+ * It is a low pass filter.
+ * Example 5 mask: (1/5) [1 1 1 1 1]
+ */
 #include "StandardFilter.h"
 #include <vector>
 template <class T>
@@ -13,7 +15,7 @@ class MeanFilter : public StandardFilter<T>{
 public:
     /** Standard Constructor
      *  The length is set to 5
-     *  We construct an array of given length with all the entries set as 1/length
+     *  We construct an array of length 5 with all the entries set as 1/length
      */
     MeanFilter();
     /** Constructor with the length of the mask
---
+++ Project/PrewittFilter_imp.h
@@ -0,0 +1,32 @@
+//
+// Created by shernand on 12/7/17.
+//
+
+template<typename T> PrewittFilter<T>::PrewittFilter() : StandardFilter<T>()
+{
+    this->setLength(5);
+    int pLength = this->length;
+    std::vector<T> pMask =this->mask;
+    pMask = std::vector<T>(pLength);
+    //\todo Find a way to implement the prewitt mas with iota o foreach
+    //Fills the mask
+    int half = pLength/2;
+    for(int i = 0; i<=half;i++){
+        pMask[i] = half - i;
+        pMask[a-1-i] = -(half - i);
+    }
+    pMask[half]=0;
+    this->setMask(pMask);
+}
+template<typename T> PrewittFilter<T>::PrewittFilter(int pLength) : StandardFilter<T>(pLength)
+{
+    this->length=pLength;
+    std::vector<T> mask = std::vector<T>(pLength);
+    int half = pLength/2;
+    for(int i = 0; i<=half;i++){
+        mask[i] = pLength - 2 * half +i;
+        mask[a-1-i] = -(pLength - 2 * half+i);
+    }
+    mask[half]=0;
+    this->setMask(mask);
+}
