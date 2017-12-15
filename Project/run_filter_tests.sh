55388c4936ad6200fc0df7c181e6bfc62fc4d8ea
Didier Bieler <didier.bieler@epfl.ch>
2017-12-15 13:05:38 +0100

    Small changes

--- Project/run_filter_tests.sh
+++ Project/run_filter_tests.sh
@@ -25,3 +25,32 @@ echo "Laplace (high pass) test ran succesfully"
 echo "Runing Prewitt (band pass) test"
 ./Project ${TEST_FILES_FOLDER}/test_prewitt_bandPass_options.txt
 echo "Prewitt (band pass) test ran succesfully"
+## Runs the Fourier domain filter tests
+echo "Running the Fourier domain filter tests"
+./TestFourierFilters
+echo "Fourier domain filter tests ran succesfully"
+##Runs the Song Test
+echo "Running Test Song"
+./TestSong
+echo "Test Song ran succesfully"
+##Runs the Fourier Transform error test
+echo "Running Fourier Transform error test"
+./TestErrorFT
+echo "Fourier Transform error test ran succesfully"
+##Runs the histogram error test
+echo "Running Histogram error test"
+./TestErrorHistogram
+echo "Histogram error test ran succesfully"
+##Runs the Fourier Transform test
+echo "Running Fourier Transform test"
+./TestFT
+echo "Fourier Transform test ran succesfully"
+##Runs the Fourier Transform test
+echo "Running test for reading the frequency file"
+./TestFrequencyFile
+echo "Test for reading the frequency file ran succesfully"
+##Runs the histogram test
+echo "Running Histogram test"
+./TestHistogram
+echo "Histogram test ran succesfully"
+
